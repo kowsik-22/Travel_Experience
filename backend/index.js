0f1025bfc09d45ca854b9c3c55ee94234a683a8a
kowsik-22 <kowsik22.a@gmail.com>
2025-06-04 16:19:21 +0530

    bugs and toast edit

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const config = require("./config.json");
 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
